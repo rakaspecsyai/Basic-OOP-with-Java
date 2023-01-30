3374e44eb9efb5caf1a3bc0d9a2b3ee29f2e22f5
Kgs. Raka Renata <rakarenata123@gmail.com>
2023-01-30 11:17:33 +0700

    updated

--- com/kampus/LinearEquation.java
+++ com/kampus/LinearEquation.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 class LinearEquation{
     private double a, b, c, d , e, f;
 
-    LinearEquation(double a, double b, double c, double d, double e, double f){
+     LinearEquation(double a, double b, double c, double d, double e, double f) {
         this.a = a;
         this.b = b;
         this.c = c;
@@ -14,64 +14,51 @@ class LinearEquation{
         this.f = f;
     }
 
-    double getA(){
-        return this.a;
+     double getA() {
+        return a;
     }
 
-    double getB(){
-        return this.b;
-
+     double getB() {
+        return b;
     }
 
-    double getC(){
-        return this.c;
-
+     double getC() {
+        return c;
     }
 
-    double getD(){
-        return this.d;
-
+     double getD() {
+        return d;
     }
 
-    double getE(){
-        return this.e;
-
+     double getE() {
+        return e;
     }
 
-    double getF(){
-        return this.f;
+     double getF() {
+        return f;
     }
 
     boolean isSolvable(){
-        return (this.a * this.d - this.b *this.c) != 0;
+        return (a*d - b*c) != 0;
     }
 
     double getX(){
-        return (this.e * this.d - this.b * this.f)/(this.a * this.d - this.b * this.c);
-        
+        return (e * d - b * f)/(a*d - b*c);
     }
 
     double getY(){
-        return (this.a * this.f - this.e * this.c)/(this.a * this.d - this.b * this.c);
-        
+        return (a*f - e*c)/(a*d - b*c);
     }
 
     void display(){
-        if(this.isSolvable()){
-            System.out.printf("(x, y) = (%.1f, %.1f)", this.getX(), this.getY());
-        }
-        else{
-            System.out.println("These equations have no solutions.");
-        }
+        
     }
-
-    
 }
 
 
 
  class Main {
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         double a = input.nextDouble();
         double b = input.nextDouble();
@@ -79,6 +66,7 @@ class LinearEquation{
         double d = input.nextDouble();
         double e = input.nextDouble();
         double f = input.nextDouble();
+        input.close();
 
         LinearEquation persamaan1 = new LinearEquation(a, b, c, d, e, f);
         persamaan1.display();
