62dcba947e11c52401bd656cd33a0c6888989b83
Kgs. Raka Renata <rakarenata123@gmail.com>
2023-02-13 13:57:50 +0700

    update

--- mini_KNN/Driver.java
+++ mini_KNN/Driver.java
@@ -1,13 +1,14 @@
 package mini_KNN;
 
-class Driver {
+public class Driver {
     public static void main(String[] args) {
-        int num_of_points = 5;
-        double[][] listXY = new double[num_of_points][2];
-
-        for (int i = 0; i < listXY.length; i++) {
-            listXY[i][0] = Math.random() * 10;
-            listXY[i][1] = Math.random() * 10;
+        double[][] listXY = {{1, 2}, {3, 4}, {7, 8}, {5, 6} };
+        KNN knn = new KNN(listXY);
+        knn.ShowPoints();
+        System.out.println(knn.isEmpty());
+        double[] distances = knn.getDistance(0, 0, true);
+        for (double d : distances) {
+            System.out.println(d);
         }
     }
 }
--- mini_KNN/KNN.java
+++ mini_KNN/KNN.java
@@ -1,10 +1,80 @@
 package mini_KNN;
 
-class KNN {
-    Point[] points;
+public class KNN {
+    private Point[] points;
 
-    public KNN(double[] listXY) {
-        this.points = points;
+    public KNN(double[][] listXY) {
+        points = new Point[listXY.length];
+        for (int i = 0; i < listXY.length; i++) {
+            points[i] = new Point(listXY[i][0], listXY[i][1]);
+        }
     }
 
+    public void ShowPoints() {
+        for (Point p : points) {
+            System.out.println(p);
+        }
+    }
+
+    public boolean isEmpty() {
+        return points.length == 0;
+    }
+
+    private double euclidean_distance(Point point, double x, double y) {
+        double dx = point.getX() - x;
+        double dy = point.getY() - y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    public double[] getDistance(double x, double y, boolean isSorted) {
+        double[] distances = new double[points.length];
+        for (int i = 0; i < points.length; i++) {
+            distances[i] = euclidean_distance(points[i], x, y);
+        }
+        if (!isSorted) {
+            distances = SortDistance(distances);
+        }
+        return distances;
+    }
+
+    private double[] SortDistance(double[] distance) {
+        int n = distance.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (distance[j] < distance[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                double temp = distance[i];
+                distance[i] = distance[minIndex];
+                distance[minIndex] = temp;
+            }
+        }
+        return distance;
+    }
+}
+
+class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" + "x = " + x + ", y = " + y + '}';
+    }
 }
