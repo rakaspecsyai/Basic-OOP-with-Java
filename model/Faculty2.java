38ed096622ee2787a66e24eb2a7e5820eedfcb3c
Kgs. Raka Renata <rakarenata123@gmail.com>
2023-02-13 22:24:34 +0700

    new file: model package

---
+++ model/Faculty2.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Faculty2 extends Employee {
+    private String rank;
+    private String officeHour;
+
+    
+
+    public Faculty2(String nama, String rank, String officeHour) {
+        this.rank = rank;
+        this.officeHour = officeHour;
+    }
+    public String getRank() {
+        return rank;
+    }
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+    public String getOfficeHour() {
+        return officeHour;
+    }
+    public void setOfficeHour(String officeHour) {
+        this.officeHour = officeHour;
+    }
+
+    
+}
