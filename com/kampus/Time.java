833eb2d10e5bcec98114df4edddf2b22b3e70301
Kgs. Raka Renata <rakarenata123@gmail.com>
2023-02-13 11:04:26 +0700

    update Time.java

--- com/kampus/Time.java
+++ com/kampus/Time.java
@@ -9,8 +9,10 @@ public class Time{
         this.second = System.currentTimeMillis()/1000;
     }
 
-    Time(int a){
-
+    Time(long elapsed){
+        this.hour = elapsed/1000/60/60;
+        this.minute = elapsed/1000/60;
+        this.second = elapsed/1000;
     }
 
     Time(long hour, long minute, long second) {
@@ -19,11 +21,40 @@ public class Time{
         this.second = second;
     }
 
+    public long getHour() {
+        return hour;
+    }
+
+    public long getMinute() {
+        return minute;
+    }
+
+    public long getSecond() {
+        return second;
+    }
+
+    void setTime(long elapseTime){
+        this.hour = elapseTime/1000/60/60;
+        this.minute = elapseTime/1000/60;
+        this.second = elapseTime/1000;
+    }
+
+    void display(){
+        System.out.printf("%d:%d:%d\n", this.getHour(), this.getMinute(), this.getSecond());
+    }
+
     
 }
 
 class TestTime{
     public static void main(String[] args) {
-        System.out.println(System.currentTimeMillis()/1000/60/60/24/365);
+        Time time1 = new Time();
+        time1.display();
+        System.out.println();
+        Time time2 = new Time(555550000);
+        time2.display();
+        System.out.println();
+        Time time3 = new Time(10, 20, 30);
+        time3.display();
     }
 }
